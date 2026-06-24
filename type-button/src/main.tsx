f07416b65057c1d4db71e2f405408c6b1d089d5e
bitpixi2 <kasey.bitpixi@gmail.com>
2026-06-24 12:51:11 +0000

    Use clearer number font

--- type-button/src/main.tsx
+++ type-button/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@fontsource/pixelify-sans/latin-500.css";
+import "@fontsource/vt323/latin-400.css";
 import { App } from "./App";
 import "./styles.css";
 
